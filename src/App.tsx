fc931c6ed90305d7c13ed0727886f543d559e481
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-05-28 23:13:17 +0700

    Divide-Page

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Portfolio from './screens/portfolio';
+
+function App() {
+
+  return (
+    <Portfolio />
+  )
+}
+
+export default App;
---
+++ src/components/preloader.tsx
@@ -0,0 +1,14 @@
+
+const PreLoader = () => {
+    return (
+        <>
+        {/* <div className="arlo_tm_preloader">
+        <div className="spinner_wrap">
+          <div className="spinner"></div>
+        </div>
+      </div> */}
+        </>
+    )
+}
+
+export default PreLoader;
